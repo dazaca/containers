2c4553e43f9ebed71c986f6d175376d2295e5105
dazaca <zafdav@gmail.com>
2022-02-05 14:46:57 +0000

    2.0 Launcher script for RStudio Server container.

--- launch_rstudio_docker.sh
+++ launch_rstudio_docker.sh
@@ -14,7 +14,7 @@ cat ~/Containers/2_docker_hub_pw.txt | sudo docker login --username dazaca --pas
 
 # Will run the container in the background with "-d" option:
 
-sudo docker run -d -p 8787:8787 -e PASSWORD=nene 9537c15e4354
+sudo docker run -d -p 8787:8787 -e PASSWORD=toto 9537c15e4354
 sleep 1 
 firejail /usr/lib/firefox/firefox http://localhost:8787/
 
