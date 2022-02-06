078c45d955f92687f6144a555c5cc67dacf54d1d
dazaca <zafdav@gmail.com>
2022-02-06 19:25:17 +0000

    Adding commands + updating 'ml' launch script.

---
+++ launch_rstudio_docker_ml.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# Copyright (c) 2022, David Zafra Cámara
+# Runs RStudio Server container based on personalized image of rocker/cuda:
+
+# https://hub.docker.com/r/rocker/cuda
+
+
+# Will Remove all stopped containers
+#docker container prune
+
+# Will Remove all exited containers from previous linux sessions
+sudo docker rm $(sudo docker ps --filter status=exited -q)
+
+# Will log with Docker Hub account by reading the pwd from the txt file:
+
+cat ~/Containers/2_docker_hub_pw.txt | sudo docker login --username dazaca --password-stdin
+
+
+
+#######################################
+# https://hub.docker.com/r/rocker/cuda
+
+
+########## Just R:
+
+#docker run --rm -ti rocker/ml R
+
+# Machines with nvidia-docker and GPU support:
+# docker run --gpus all --rm -ti rocker/ml R
+
+########## RStudio:
+
+# Machines with nvidia-docker and GPU support:
+#docker run --gpus all -e PASSWORD=mu -p 8787:8787 rocker/ml
+
+# Will run the container in the background with "-d" option:
+#sudo docker run -d -p 8787:8787 -e PASSWORD=toto 8ae0c09f87a2
+#sudo docker run -d -e PASSWORD=toto -p 8787:8787 rocker/ml
+#sudo docker run -d -p 8787:8787 -e PASSWORD=toto 96bf9452de2a 
+sudo docker run -d -p 8787:8787 -e PASSWORD=toto rocker/ml-verse 
+
+sleep 1
+firejail /usr/lib/firefox/firefox http://localhost:8787/
